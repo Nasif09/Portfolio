d8cc95212b68565f469a40b62f75ee0768f58bc8
Nasif Rahman <113826897+Nasif09@users.noreply.github.com>
2026-01-24 01:39:44 +0600

    update

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 $(document).ready(function () {
+
+    // Menu toggle
     $('#menu').click(function () {
         $(this).toggleClass('fa-times');
         $('header').toggleClass('toggle');
     });
 
+    // Scroll & load behavior
     $(window).on('scroll load', function () {
         $('#menu').removeClass('fa-times');
         $('header').removeClass('toggle');
@@ -14,15 +17,28 @@ $(document).ready(function () {
             $('.top').hide();
         }
     });
-    //smooth scrolling
+
+    // Smooth scrolling
     $('a[href*="#"]').on('click', function (e) {
         e.preventDefault();
-        $('html,body').animate({
-            scrollTop: $($(this).attr('href')).offset().top,
-        },
+        $('html, body').animate(
+            {
+                scrollTop: $($(this).attr('href')).offset().top,
+            },
             500,
             'linear'
         );
     });
 
-}); 
+    // ===============================
+    // View More / View Less Projects
+    // ===============================
+    $('#viewMoreBtn').click(function () {
+        $('.extra-project').slideToggle(400);
+
+        $(this).text(function (i, text) {
+            return text === "View More" ? "View Less" : "View More";
+        });
+    });
+
+});
